5b3947d29ead9b5768ab4d9c39ebd51a2256050e
StarGrim33 <108456723+StarGrim33@users.noreply.github.com>
2023-08-31 14:16:40 +0300

    added movement scripts for player

---
+++ TempleRunner/Assets/Source/Scripts/Player/PlayerContoller.cs
@@ -0,0 +1,58 @@
+using System;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController), typeof(SwipeMovement))]
+public class PlayerContoller : MonoBehaviour
+{
+    private SwipeMovement _swipeMovement;
+    private CharacterController _controller;
+    private Vector3 _direction;
+    private float _speed = 5f;
+    private int _lineToMove = 1;
+    private float _jumpForce = 10f;
+    private float _gravity = -20f;
+
+    public float LineDistance => 4;
+
+    private void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+        _swipeMovement = GetComponent<SwipeMovement>();
+    }
+
+    private void Update()
+    {
+        if(_swipeMovement.SwipeRight)
+            if(_lineToMove < 2)
+                _lineToMove++;
+
+        if(_swipeMovement.SwipeLeft)
+            if(_lineToMove > 0) 
+                _lineToMove--;
+
+        if (_swipeMovement.SwipeUp)
+            if(_controller.isGrounded)
+            Jump();
+
+        Vector3 targetPosition = transform.position.z * transform.forward + transform.position.y * transform.up;
+
+        if (_lineToMove == 0)
+            targetPosition += Vector3.left * LineDistance;
+        else if( _lineToMove == 2)
+            targetPosition += Vector3.right * LineDistance;
+
+        transform.position = targetPosition;
+    }
+
+    private void Jump()
+    {
+        _direction.y = _jumpForce;
+    }
+
+    private void FixedUpdate()
+    {
+        _direction.z = _speed;
+        _direction.y += _gravity * Time.fixedDeltaTime;
+        _controller.Move(_direction * Time.fixedDeltaTime);
+    }
+}
---
+++ TempleRunner/Assets/Source/Scripts/Player/SwipeMovement.cs
@@ -0,0 +1,101 @@
+using UnityEngine;
+
+public class SwipeMovement : MonoBehaviour
+{
+    public bool Tap { get; private set; }
+
+    public bool SwipeLeft { get; private set; }
+
+    public bool SwipeRight { get; private set; }
+
+    public bool SwipeUp { get; private set; }
+
+    public bool SwipeDown { get; private set; }
+
+    private bool isDraging = false;
+    private Vector2 startTouch, swipeDelta;
+
+    private void Update()
+    {
+        Tap = SwipeDown = SwipeUp = SwipeLeft = SwipeRight = false;
+
+        ProcessInput();
+        CalculateSwipe();
+
+        if (swipeDelta.magnitude > 100)
+        {
+            DetermineSwipeDirection();
+            ResetSwipeData();
+        }
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Tap = true;
+            isDraging = true;
+            startTouch = Input.mousePosition;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            isDraging = false;
+            ResetSwipeData();
+        }
+
+        if (Input.touches.Length > 0)
+        {
+            if (Input.touches[0].phase == TouchPhase.Began)
+            {
+                Tap = true;
+                isDraging = true;
+                startTouch = Input.touches[0].position;
+            }
+            else if (Input.touches[0].phase == TouchPhase.Ended || Input.touches[0].phase == TouchPhase.Canceled)
+            {
+                isDraging = false;
+                ResetSwipeData();
+            }
+        }
+    }
+
+    private void CalculateSwipe()
+    {
+        swipeDelta = Vector2.zero;
+
+        if (isDraging)
+        {
+            if (Input.touches.Length > 0)
+                swipeDelta = Input.touches[0].position - startTouch;
+            else if (Input.GetMouseButton(0))
+                swipeDelta = (Vector2)Input.mousePosition - startTouch;
+        }
+    }
+
+    private void DetermineSwipeDirection()
+    {
+        float x = swipeDelta.x;
+        float y = swipeDelta.y;
+
+        if (Mathf.Abs(x) > Mathf.Abs(y))
+        {
+            if (x < 0)
+                SwipeLeft = true;
+            else
+                SwipeRight = true;
+        }
+        else
+        {
+            if (y < 0)
+                SwipeDown = true;
+            else
+                SwipeUp = true;
+        }
+    }
+
+    private void ResetSwipeData()
+    {
+        startTouch = swipeDelta = Vector2.zero;
+        isDraging = false;
+    }
+}
